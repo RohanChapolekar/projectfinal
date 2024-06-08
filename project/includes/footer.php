fa14c2f0acdd9051ad4d8da42e49034552cec501
RohanChapolekar <chapolekarrohan@gmail.com>
2024-06-08 11:46:23 +0530

    Add files via upload

---
+++ project/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | Employee Leave Management System in PHP | Developed By Rohan chapolekar</p>
+        </div>
+</footer>
